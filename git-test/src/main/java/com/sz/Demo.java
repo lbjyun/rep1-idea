c38f0ab2b9853c250a7cb6ac86f4015827323920
wangwu <wangwu@qq.com>
2019-09-02 19:46:37 +0800

    提交到本地仓库

---
+++ git-test/src/main/java/com/sz/Demo.java
@@ -0,0 +1,21 @@
+package com.sz;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello sz ");
+        System.out.println("hello sz ");
+        System.out.println("hello sz ");
+        System.out.println("hello sz ");
+        System.out.println("hello sz ");
+        System.out.println("hello sz ");
+        System.out.println("hello sz ");
+        System.out.println("hello sz ");
+        System.out.println("hello sz ");
+        System.out.println("hello sz ");
+        System.out.println("hello sz ");
+        System.out.println("hello sz ");
+        System.out.println("hello sz ");
+    }
+}
