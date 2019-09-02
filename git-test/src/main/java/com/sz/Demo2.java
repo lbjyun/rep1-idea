1fc653984bdfb473680b62070f200ad9befc091d
wangwu <wangwu@qq.com>
2019-09-02 20:35:54 +0800

    添加Demo2的类

---
+++ git-test/src/main/java/com/sz/Demo2.java
@@ -0,0 +1,21 @@
+package com.sz;
+
+public class Demo2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("ssssssss");
+
+        System.out.println("今晚打老虎");
+        method();
+    }
+
+    public static void method(){
+        int i=10;
+        int j=20;
+
+        System.out.println(i+j);
+
+        //System.out.println("");
+    }
+}
